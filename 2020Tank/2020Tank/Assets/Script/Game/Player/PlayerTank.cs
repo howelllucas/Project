b71fe12dc517ef0ef71b78b90fac49ebf35459fc
howelllucas <howelllucas1982@gmail.com>
2020-12-23 19:15:48 +0800

    tank2020

---
+++ 2020Tank/2020Tank/Assets/Script/Game/Player/PlayerTank.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank : MonoBehaviour
+{
+    Vector2 moveDir;
+    private Rigidbody rid;
+    public float moveSpeed = 8.0f;
+    void Awake()
+    {
+        rid = this.transform.GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal")!=0||Input.GetAxis("Vertical")!=0)
+        {
+            moveDir.x = Input.GetAxis("Horizontal");
+            moveDir.y = Input.GetAxis("Vertical");
+            Move(moveDir);
+        }
+        
+    }
+
+    void Move(Vector2 dir)
+    {
+        //旋转
+        Vector3 rot = new Vector3(moveDir.x, 0, moveDir.y);
+        this.transform.rotation = Quaternion.LookRotation(rot);
+        //移动
+        Vector3 Dir = this.transform.forward * moveSpeed * Time.deltaTime;
+        rid.MovePosition(this.transform.position+ Dir);
+    }
+}

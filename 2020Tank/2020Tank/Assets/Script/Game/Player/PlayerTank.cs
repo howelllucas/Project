ad481c4c8e7c158e7fd8b037c81615d9a3785fcf
howelllucas <howelllucas1982@gmail.com>
2020-12-24 18:34:07 +0800

    1224

--- 2020Tank/2020Tank/Assets/Script/Game/Player/PlayerTank.cs
+++ 2020Tank/2020Tank/Assets/Script/Game/Player/PlayerTank.cs
@@ -7,6 +7,14 @@ public class PlayerTank : MonoBehaviour
     Vector2 moveDir;
     private Rigidbody rid;
     public float moveSpeed = 8.0f;
+    public GameObject bulletPrefab;
+    public Transform bulletCreatPoint;
+    //坦克开火特效预制体
+    public GameObject firePrefab;
+    //子弹间隔时间
+    float bulletTime=0.5f;
+    //下一次发射时间
+    float nextTime=0 ;
     void Awake()
     {
         rid = this.transform.GetComponent<Rigidbody>();
@@ -14,7 +22,7 @@ public class PlayerTank : MonoBehaviour
 
     void Start()
     {
-        
+        Camera.main.GetComponent<FollowTarget>().Target = this.transform;
     }
 
     
@@ -25,6 +33,10 @@ public class PlayerTank : MonoBehaviour
             moveDir.x = Input.GetAxis("Horizontal");
             moveDir.y = Input.GetAxis("Vertical");
             Move(moveDir);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            creatBullet();
         }
         
     }
@@ -37,5 +49,16 @@ public class PlayerTank : MonoBehaviour
         //移动
         Vector3 Dir = this.transform.forward * moveSpeed * Time.deltaTime;
         rid.MovePosition(this.transform.position+ Dir);
+    }
+    void creatBullet()
+    {
+        bulletPrefab.GetComponent<bullet>().Owner = this.gameObject;
+        if (Time.time> nextTime)
+        {
+            GameObject.Instantiate(firePrefab, bulletCreatPoint.position, Quaternion.identity);
+            GameObject.Instantiate(bulletPrefab, bulletCreatPoint.position, bulletCreatPoint.rotation);
+            nextTime = Time.time+ bulletTime;
+        }
+        
     }
 }
---
+++ 2020Tank/2020Tank/Assets/Script/Game/enemy/enemyTank.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyTank: MonoBehaviour
+{
+    //实时距离
+    private float distance;
+    //追击距离
+    private float followDistance;
+    //攻击距离
+    private float attackDistance;
+    //目标玩家坦克
+    private GameObject playerTank;
+    //旋转速度
+    private float rotationSpeed = 5.0f;
+    //移动速度
+    private float moveSpeed = 5.0f;
+    void Start()
+    {
+        playerTank = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    
+    void Update()
+    {
+        distance = Vector3.Distance(this.transform.position, playerTank.transform.position);
+        if (distance<followDistance)
+        {
+            //追击
+            followMove();
+        }
+        else if (distance<attackDistance)
+        {
+            //攻击
+        }
+        else
+        {
+
+        }
+    }
+    //追击方法
+    void followMove()
+    {
+        //旋转
+        Vector3 rot = playerTank.transform.position-this.transform.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(rot),Time.deltaTime*rotationSpeed)；
+        //移动
+        this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}

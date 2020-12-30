3a7318ef11e247a42fbe7339bda9daf6344f83cd
lucas <howelllucas@gmail.com>
2020-12-30 09:01:47 +0800

    1230zao

---
+++ 2020Tank/2020Tank/Assets/Script/Game/FXDestory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXDestory : MonoBehaviour
+{
+    ParticleSystem particle;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        particle = this.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (particle.isPlaying!=true)
+        {
+            GameObject.Destroy(this);
+        }
+    }
+}

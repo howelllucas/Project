aad1e74111a13b0237891f61e96dc7d66681da11
howelllucas <howelllucas1982@gmail.com>
2021-01-06 19:16:39 +0800

    TD添加地图

---
+++ Tower Defense/Assets/Import Assets/Scripts/enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ns
+{
+    ///<summary>
+    ///
+    ///</summary>
+    public class enemy : MonoBehaviour
+    {
+        int Speed = 10;
+        Transform[] wayTrans;
+        int index = 0;
+        Rigidbody rid;
+        private void Awake()
+        {
+            wayTrans = wayPoint.tans;
+            rid = this.transform.GetComponent<Rigidbody>();
+        }
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (index> wayTrans.Length-1)
+            {
+                return;
+            }
+            this.transform.LookAt(wayTrans[index + 1]);
+            Vector3 Dir = this.transform.forward * Speed * Time.deltaTime;
+            rid.MovePosition(this.transform.position + Dir);
+            if (Vector3.Distance(this.transform.position, wayTrans[index+1].position)<0.2f)
+            {
+                index++;
+            }
+            
+        }
+
+    }
+
+}
+
---
+++ Tower Defense/Assets/Import Assets/Scripts/wayPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+    ///<summary>
+    ///
+    ///</summary>
+    public class wayPoint : MonoBehaviour
+    {
+        public static Transform[] tans;
+        
+        void Awake()
+        {
+            tans = new Transform[transform.childCount];
+            
+            for (int i = 0; i < tans.Length; i++)
+            {
+                tans[i] = this.transform.GetChild(i);
+            }
+            
+        }
+    }
+
+}
+

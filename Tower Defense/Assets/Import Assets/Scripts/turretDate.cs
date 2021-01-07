c4a308f43cbde2b2b03e048a5b4af1b2ad75975b
howelllucas <howelllucas1982@gmail.com>
2021-01-07 19:29:31 +0800

    0107晚

---
+++ Tower Defense/Assets/Import Assets/Scripts/turretDate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+    ///<summary>
+    ///
+    ///</summary>
+    public class turretDate 
+    {
+        public GameObject turretPrefab;
+        public int cost;
+        public GameObject turretUpPrefab;
+        public int costUp;
+        public turretType type;
+
+    }
+    public enum turretType
+    {
+        LaserTurret,
+        MissileTurret,
+        StandardTurret
+    }
+
+}
+

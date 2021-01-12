b41336a7e6ad72853f419f089954cb273c0d7534
lucas <howelllucas@gmail.com>
2021-01-13 00:26:22 +0800

    0113-1

---
+++ Tower Defense/Assets/Import Assets/Scripts/UIBillboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//ui看向摄像机
+public class UIBillboard : MonoBehaviour
+{
+    /// <summary>
+    /// 摄像机变换对象
+    /// </summary>
+    private Transform camTrans;
+
+    /// <summary>
+    /// 跟随对象
+    /// </summary>
+    private Transform trans;
+
+    void Awake()
+    {
+        camTrans = Camera.main.transform;
+        trans = transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(trans.position + camTrans.rotation * Vector3.forward, camTrans.rotation * Vector3.up);
+    }
+}

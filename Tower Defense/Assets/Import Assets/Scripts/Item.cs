2371f11b1c7f55b0136468a562b4fea202d071b0
howelllucas <howelllucas1982@gmail.com>
2021-01-18 20:04:50 +0800

    0118晚

---
+++ Tower Defense/Assets/Import Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+    ///<summary>
+    ///
+    ///</summary>
+    [System.Serializable]
+    public class Item 
+    {
+
+        public string itemId;
+        public string itemHP;
+        public string itemSpeed;
+        public string itemCast;
+
+    }
+
+}
+
---
+++ Tower Defense/Assets/Import Assets/Scripts/ItemManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace ns
+{
+    ///<summary>
+    ///
+    ///</summary>
+    [System.Serializable]
+    public class ItemManager : ScriptableObject
+    {
+        public Item[] dataArray;
+    }
+
+
+}
+
